dafa4929ee6b7f5344736787f27f0b75300dc45d
Patrick Adams <padams@sdfiana024.sdf.slac.stanford.edu>
2025-07-17 01:47:24 -0700

    added proc run

--- PA-stuff/constants.py
+++ PA-stuff/constants.py
@@ -5,11 +5,12 @@
 
 
 DET_NAME = 'CxiDs1.0:Jungfrau.0'
-DET_SHAPE = (8, 1024, 512)
+DET_SHAPE = (8, 512, 1024)
 
 EXP_NAME ='cxi100844924'
-EXP_NAME = 'cxip18619'
+#EXP_NAME = 'cxip18619'
 EXP_FOLDER = f'/sdf/data/lcls/ds/cxi/{EXP_NAME}/'
+H5_FOLDER = f'{EXP_FOLDER}/results/h5out'
 
 
 
