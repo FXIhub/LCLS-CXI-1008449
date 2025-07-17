61287f8d97bf5ebda3a4886d72b9f10576f392ad
Patrick Adams <padams@sdfiana023.sdf.slac.stanford.edu>
2025-07-16 17:18:29 -0700

    om_scripts

--- om_example/run_gui.sh
+++
@@ -1,4 +0,0 @@
-source /sdf/group/lcls/ds/ana/sw/conda1/manage/bin/psconda.sh
-source ~/software/install/bin/activate-om
-#om_swaxs_gui.py tcp://172.24.49.222:12321
-om_swaxs_gui.py tcp://sdfiana023:12321
